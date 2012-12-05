f401db0a9fe2047388fff24bad1250d81d3013de
vincentx <vincent.hao.xu@gmail.com>
2012-12-05 15:11:15 +0800

    add plugin to repository

---
+++ i0-gradle-plugin/src/main/groovy/com/thoughtworks/i0/plugins/gradle/I0Plugin.groovy
@@ -0,0 +1,11 @@
+package com.thoughtworks.i0.plugins.gradle
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vincentx
+ * Date: 12/5/12
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class I0Plugin {
+}
