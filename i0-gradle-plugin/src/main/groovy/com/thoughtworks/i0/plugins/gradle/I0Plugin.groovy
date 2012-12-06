011fc9d515ee4a20a200221b441c54da3e27dc25
vincentx <vincent.hao.xu@gmail.com>
2012-12-06 10:31:05 +0800

    the i0 plugin now will add dependencies

--- i0-gradle-plugin/src/main/groovy/com/thoughtworks/i0/plugins/gradle/I0Plugin.groovy
+++ i0-gradle-plugin/src/main/groovy/com/thoughtworks/i0/plugins/gradle/I0Plugin.groovy
@@ -1,11 +1,23 @@
 package com.thoughtworks.i0.plugins.gradle
 
-/**
- * Created with IntelliJ IDEA.
- * User: vincentx
- * Date: 12/5/12
- * Time: 2:46 PM
- * To change this template use File | Settings | File Templates.
- */
-class I0Plugin {
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.plugins.JavaPlugin
+
+class I0Plugin implements Plugin<Project> {
+
+    final static def JAVAX_DEPENDENCIES = ['javax.servlet:servlet-api:3.0', 'javax.inject:javax.inject:1',
+            'javax.annotation:jsr-305:2.0.1']
+
+    @Override
+    void apply(Project project) {
+        project.repositories {
+            mavenCentral()
+            mavenRepo(url: 'https://github.com/vincentx/i0/raw/master/repository')
+        }
+        project.plugins.apply(JavaPlugin.class)
+        project.dependencies {
+            JAVAX_DEPENDENCIES.each {compile it}
+        }
+    }
 }
