076e6f6253dc73cbdfa8ed04655e8d6b47f0d274
vincentx <vincent.hao.xu@gmail.com>
2012-12-06 11:21:32 +0800

    update gradle plugin

--- i0-gradle-plugin/src/main/groovy/com/thoughtworks/i0/plugins/gradle/I0Plugin.groovy
+++ i0-gradle-plugin/src/main/groovy/com/thoughtworks/i0/plugins/gradle/I0Plugin.groovy
@@ -7,7 +7,7 @@ import org.gradle.api.plugins.JavaPlugin
 class I0Plugin implements Plugin<Project> {
 
     final static def JAVAX_DEPENDENCIES = ['javax.servlet:servlet-api:3.0', 'javax.inject:javax.inject:1',
-            'javax.annotation:jsr-305:2.0.1']
+            'javax.annotation:jsr305:2.0.1']
 
     final static def FUNCTIONAL_JAVA = ['com.google.guava:guava:13.0.1']
 
@@ -21,7 +21,9 @@ class I0Plugin implements Plugin<Project> {
         project.dependencies {
             JAVAX_DEPENDENCIES.each {compile it}
             FUNCTIONAL_JAVA.each {compile it}
-            compile 'com.thoughtworks.i0:i0-core:0.1.0'
+            compile 'com.thoughtworks.i0:i0-core:0.1.0' {
+                transitive = false
+            }
         }
     }
 }
