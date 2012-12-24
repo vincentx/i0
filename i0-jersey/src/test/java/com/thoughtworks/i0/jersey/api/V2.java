905d2b18a25429acfd11123f0e0ba77222c6b0f9
vincentx <vincent.hao.xu@gmail.com>
2012-12-24 23:37:07 +0800

    add test for changing prefix

---
+++ i0-jersey/src/test/java/com/thoughtworks/i0/jersey/api/V2.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.i0.jersey.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vincentx
+ * Date: 12/24/12
+ * Time: 11:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class V2 {
+}
