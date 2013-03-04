9ef8d88849446d84c5b8d2b70aa2440d126cd23c
aqingsao <xqzhang@thoughtworks.com>
2013-03-04 09:25:41 +0800

    Enable JacksonJaxbJson and disable FEATURE_POJO_MAPPER

--- i0-jersey/src/main/java/com/thoughtworks/i0/jersey/internal/JerseyEnabler.java
+++ i0-jersey/src/main/java/com/thoughtworks/i0/jersey/internal/JerseyEnabler.java
@@ -9,6 +9,9 @@ import com.thoughtworks.i0.config.Configuration;
 import com.thoughtworks.i0.core.ApplicationModule;
 import com.thoughtworks.i0.core.BindingProvider;
 import com.thoughtworks.i0.jersey.RestApi;
+import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+
+import javax.inject.Singleton;
 
 import static com.google.common.base.Joiner.on;
 import static com.sun.jersey.api.core.PackagesResourceConfig.PROPERTY_PACKAGES;
@@ -22,10 +25,11 @@ public class JerseyEnabler implements BindingProvider<RestApi, Configuration> {
         binder.install(new JerseyServletModule() {
             @Override
             protected void configureServlets() {
+                bind(JacksonJaxbJsonProvider.class).in(Singleton.class);
                 ImmutableSet<String> packageSet = ImmutableSet.<String>builder()
                         .add(annotation.packages().length == 0 ? autoScanPackages : annotation.packages()).build();
                 serve(annotation.prefix()).with(GuiceContainer.class, new ImmutableMap.Builder<String, String>()
-                        .put(PROPERTY_PACKAGES, on(";").skipNulls().join(packageSet)).put(FEATURE_POJO_MAPPING, "true").build());
+                        .put(PROPERTY_PACKAGES, on(";").skipNulls().join(packageSet)).build());
             }
         });
     }
